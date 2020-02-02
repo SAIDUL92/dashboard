112dc7ae85d2c07a318b0d0508348d2516ddec21
saidul-islam <isaidul92@gmail.com>
2020-02-02 12:35:37 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+(function($) {
+    "use strict";
+
+    $('.sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+    });
+    $('.slimscroll').slimscroll({
+        width: '100%',
+        height: '100% - 69px'
+
+    });
+    // sticky
+    var wind = $(window);
+    var sticky = $('header');
+    wind.on('scroll', function() {
+        var scroll = wind.scrollTop();
+        if (scroll < 100) {
+            sticky.removeClass('sticky');
+        } else {
+            sticky.addClass('sticky');
+        }
+    });
+    // NiceSeclectore
+    $('.select').niceSelect();
+
+
+})(jQuery);
